9201fd5449b17e942eade72aae0d889b61e23525
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-09-14 14:01:16 +0900

    First commit to connect with GitHub

---
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.composecourseyt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting(name = "Kang")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCourseYTTheme {
+        Greeting("Kang")
+    }
+}
