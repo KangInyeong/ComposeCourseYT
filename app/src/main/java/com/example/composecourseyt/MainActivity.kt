d04a0b0eba737550b52ed8653740dcf904d24079
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-09-28 10:45:07 +0900

    Fifth commit after studying Styling Text - Android Jetpack Compose - Part 5 video

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -22,7 +22,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,65 +40,48 @@ import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //4차시
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_thin, FontWeight.Thin),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold)
+        )
+        //5차시
         setContent {
-            val painter = painterResource(id = R.drawable.disney)
-            val description = "This is the disney castle"
-            val title = "Disney is the real"
-
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f) // 카드의 크기가 화면 절반만 채우기
-                .padding(16.dp)
-            ) {
-                ImageCard(
-                    painter = painter,
-                    contentDescription = description,
-                    title = title
+            Box(modifier = Modifier.fillMaxSize().background(Color(0xFF101010))) {
+                Text(
+                    text = buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color = Color.Blue,
+                                                        fontSize = 50.sp
+                                                    )
+                                                ){
+                                                    append("J")
+                                                }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
                 )
             }
-        }
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter : Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier= Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp), //둥근 모서리
-        elevation = 5.dp // 그림자 만들기
-    ) {
-        // 이미지 용도 박스
-        Box(modifier = Modifier.height(200.dp)) {
-            Image(
-                painter = painter, contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-
-            Box(modifier = Modifier
-                .fillMaxSize(0.5f)) {
-                    Brush.verticalGradient( //위아래로 변하는 그라데이션
-                    colors = listOf(
-                        Color.Transparent,
-                        Color.Black
-                    //아래는 어둡고 위는 투명하게 설정
-                    ),
-                    startY =  300f
-                )
-            }
-            // 이미지 위에 텍스트를 가지는 용도 박스
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-                contentAlignment = Alignment.BottomStart //텍스트를 맨 아래에
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-
         }
     }
 }
