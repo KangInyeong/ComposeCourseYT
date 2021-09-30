100c98bdf1fbd6b0e2d6ebf4c4360c43c5f79558
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-09-30 14:19:20 +0900

    Seventh commit after studying Textfields, Buttons & Showing Snackbars - Android Jetpack Compose - Part 7 video

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -9,13 +9,11 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -39,51 +37,56 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //6차시
+        //7차시 - 텍스트 필드 만들기
         setContent {
-            Column(Modifier.fillMaxSize()) {
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-                ColorBox(
-                    Modifier.weight(1f).fillMaxSize()
-                ){
-                    color.value = it //새로운 색상 지정
-                    }
-                Box(modifier = Modifier
-                    .background(color.value)
-                    .weight(1f)
-                    .fillMaxSize())
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember { // to prevent the value is reset on recomposition
+                mutableStateOf("")
             }
+            val scope = rememberCoroutineScope()
+
+                //scaffold는 layering compose이며, Jetpack compose에 이미 존재하는 material design composnent을 포함.
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ){
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(horizontal = 30.dp)
+                    ){
+                        TextField(
+                            value = textFieldState,
+                            label = {
+                                Text("Enter your name")
+                            },
+                            onValueChange = {
+                                textFieldState = it
+                            },
+                            singleLine = true,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = {
+                            scope.launch { //Warning! Never launch a coroutine directly in the composable, it's only okay in callbacks such as an onClickListener
+                                scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                            }
+                        }) {
+                            Text("Please greet me")
+                        }
+                    }
+                }
+
 
         }
     }
 }
 
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    updateColor: (Color) -> Unit
-) {
-
-    Box(modifier = modifier
-        .background(Color.Gray)
-        .clickable{
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
-        }
-
-    )
-    
-}
