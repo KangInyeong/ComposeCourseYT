4f8aab7eb4f4456eff5314f90bfd7583e069626d
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-10-06 10:04:42 +0900

    Tenth commit after studying How to Make an Animated Circular Progress Bar in Jetpack Compose - Android Studio Tutorial video

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -13,9 +13,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +20,8 @@ import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
@@ -38,6 +37,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -52,61 +53,65 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //11차시 - 단순한 애니메이션 구현
-
-        /**
-         * Box 안에 버튼이 있는데 버튼을 누르면 Box의 크기가 50dp씩 커진다.
-         * animateDpAsState를 사용해서 smoothly 하게 크기를 증가시킬 수 있다.
-         */
+        //12차시 - 프로그래스 바 ( canvas 커스텀 )
 
         setContent {
-            var sizeState by remember {mutableStateOf(200.dp)}
-            val size by animateDpAsState(
-                //want to animate towards owr size state
-                targetValue = sizeState,
-//                tween(
-//                    // 300milliseconds 뒤에 시작해서 3000milliseconds 동안 커짐.
-//                    durationMillis = 3000,
-//                    delayMillis = 300,
-//                    easing = LinearOutSlowInEasing
-//                )
-
-//                spring(Spring.DampingRatioHighBouncy) // 푸딩처럼 팝핑하면서 커짐.
-
-//                keyframes {
-////                처음에는 빠르게, 그 다음 느리게
-//                    durationMillis = 5000
-//                    sizeState at 0 with LinearEasing
-//                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-//                    sizeState * 2f at 5000
-//                }
-
-                tween(
-                    durationMillis = 1000
-                )
-            )
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(durationMillis = 2000),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
-
-            Box(modifier = Modifier
-                .size(size)
-                .background(color),
-                contentAlignment = Alignment.Center){
-                Button(onClick = {
-                    sizeState += 50.dp
-                })
-                {
-                    Text("Increase Size")
-                }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ){
+                CircularProgressBar(percentage = 0.7f, number = 100)
             }
-
         }
     }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp, //두께
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false) //애니메이션 시작할 때 boolean 사용
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if(animationPlayed) percentage else 0f, //0%에서 시작해서 animationPlayed가 true가 되면 percentage 반환
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+
+    LaunchedEffect(key1 = true){
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ){
+        Canvas(modifier = Modifier.size(radius * 2f)){
+            drawArc(
+                color = color,
+                -90f,//시작각도
+                360 * curPercentage.value,//전체각도
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(
+            text = (curPercentage.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+
 }
