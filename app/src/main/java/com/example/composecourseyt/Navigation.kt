000914248701fb918f47615a3984e128bcf5a944
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-10-08 17:55:35 +0900

    Eleventh+ commit during studying Jetpack Compose Navigation for Beginners - Android Studio Tutorial ; not yet existing error in Navigation.kt file!

--- app/src/main/java/com/example/composecourseyt/Navigation.kt
+++ app/src/main/java/com/example/composecourseyt/Navigation.kt
@@ -18,18 +18,18 @@ import androidx.navigation.compose.rememberNavController
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Screen.MainScreen.route){
-        composable(route = Screen.MainScreen.route){
-            MainScreen(navController = navController)
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) { //화면을 MainScreen에서 시작
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController) //MainScreen을 대체하여 보여질 것
         }
         composable(
-            route = Screen.DetailScreen.route + "/{name}", //mandatory argument이며 path 설정 가능
-        arguments = listOf(
-                    navArgument("name"){
-                        type = NavType.StringType
-                        defaultValue = "inyeong"
-                        nullable = true
-                    }
+            route = Screen.DetailScreen.route + "/{name}", // inyoeng이라는 name을 navigation으로 argument로서 DetailScreen에 전달, multiple argument를 원하면 슬래시와 함께 추가. ?name={name} 형식으로 optinal argument로 표현할 수도 있음.
+            arguments = listOf(
+                navArgument("name"){
+                    type = NavType.StringType
+                    defaultValue = "inyeong"
+                    nullable = true
+    }
         )
         ){ entry ->
             DetailScreen(name = entry.arguments?.getString("name"))
@@ -37,10 +37,11 @@ fun Navigation() {
     }
 }
 
+
 @Composable
-fun MainScreen(navController: NavController) {
+fun MainScreen(navController: NavController) { //버튼과 상세 스크린을 포함하는 NavController를 인스턴스로 가져야함
     var text by remember {
-        mutableStateOf("")
+        mutableStateOf("") //빈 String으로 시작. getValue와 setValue를 import 설정
     }
     Column(
         verticalArrangement = Arrangement.Center,
@@ -48,14 +49,18 @@ fun MainScreen(navController: NavController) {
             .fillMaxWidth()
             .padding(horizontal = 50.dp)
     ) {
-        TextField(value = text, onValueChange = {
-            text = it
+        TextField(value = text,
+            onValueChange = {
+            text = it //값이 바뀌면 test를 새로운 String으로 업데이트
         },
-        modifier = Modifier.height(8.dp))
-        Button(onClick = {
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
                          navController.navigate(Screen.DetailScreen.withArgs(text))
-                         },
-        modifier = Modifier.align(Alignment.End)
+                         }, // onClick 함수가 작동하면 DetailScreen으로 navigae함
+            modifier = Modifier.align(Alignment.End)
         ) {
             Text(text = "To DetailScreen")
 
@@ -66,7 +71,7 @@ fun MainScreen(navController: NavController) {
 }
 
 @Composable
-fun DetailScreen(name: String?) {
+fun DetailScreen(name: String?) { //DetailScreen은 name이 nullable되도록 하는 argument를 가짐
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
--- app/src/main/java/com/example/composecourseyt/Screen.kt
+++ app/src/main/java/com/example/composecourseyt/Screen.kt
@@ -4,9 +4,9 @@ sealed class Screen(val route: String){
     object MainScreen : Screen("main_screen")
     object DetailScreen : Screen("detail_screen")
 
-    fun withArgs(vararg args: String): String{
+    fun withArgs(vararg args: String): String{ //String으로 받아서 resulting route의 String으로 리턴
         return buildString {
-            append(route)
+            append(route) //현재 screen의 route를 append
             args.forEach { arg ->
                 append("/$arg")
             }
