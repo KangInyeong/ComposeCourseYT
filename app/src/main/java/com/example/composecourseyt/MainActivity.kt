45e9eca35ac2f713ef87b6be274e4a8fa20ad18a
KangInyeong <52899340+KangInyeong@users.noreply.github.com>
2021-10-12 00:53:17 +0900

    Twelfth commit after studying How to Make A Bottom Navigation With Badges in Jetpack Compose - Android Studio Tutorial

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -13,8 +13,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Brush
@@ -45,20 +50,175 @@ import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //네비게이션 -> build.gradle 파일에 코드 추가
 
         setContent {
-            Navigation()
+
+            ComposeCourseYTTheme{
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Default.Home
+                                ),
+
+                                BottomNavItem(
+                                    name = "Chat",
+                                    route = "chat",
+                                    icon = Icons.Default.Notifications,
+                                    badgeCount = 23 // 뱃지 클릭 시에 뜨는 숫자
+                                ),
+
+                                BottomNavItem(
+                                    name = "Settings",
+                                    route = "settings",
+                                    icon = Icons.Default.Settings,
+                                    badgeCount = 214 // 뱃지 클릭 시에 뜨는 숫자
+                                )
+                            ), navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )
+
+                    }
+                ) {
+                    Navigation(navController = navController)
+                }
+            }
+
+        }
         }
     }
+
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    //NavHost로 연결
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home"){
+            HomeScreen()
+        }
+        composable("chat"){
+            ChatScreen()
+        }
+        composable("settings"){
+            SettingsScreen()
+        }
+    }
+
+}
+
+@ExperimentalMaterialApi
+@Composable
+fun BottomNavigationBar(
+    //BottomNavItem을 만든 뒤, items의 리스트를 받아
+    items : List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemClick: (BottomNavItem) -> Unit
+){
+    val backStackEntry = navController.currentBackStackEntryAsState() // navController 사용 state 관찰
+
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp
+    ){
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route // route가 변경되면 업데이트
+            BottomNavigationItem(
+                selected = selected, // item.route == navController.currentDestination?.route였는데 selected 생성 후 변경
+                onClick = { onItemClick(item) },
+                selectedContentColor = Color.Green, // 선택 시 색상
+                unselectedContentColor = Color.Gray, // 선택 X 시 색상
+                icon = {
+                    Column(horizontalAlignment = CenterHorizontally){
+                        if(item.badgeCount > 0){
+                            BadgeBox (
+                                badgeContent = {
+                                    Text(text = item.badgeCount.toString())
+                                }
+                            ){
+                                Icon(
+                                    imageVector = item.icon,
+                                    contentDescription = item.name
+                                )
+                            }
+                        }else{
+                            Icon(
+                                imageVector = item.icon,
+                                contentDescription = item.name
+                            )
+
+                        }
+
+                        if(selected){
+                            Text(
+                                text = item.name,
+                                textAlign = TextAlign.Center,
+                                fontSize = 10.sp
+                            )
+                        }
+                    }
+            })
+        }
+    }
+}
+
+// HomeScreen 화면 UI
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Home screen")
+    }
+
+}
+
+// ChatScreen 화면 UI
+@Composable
+fun ChatScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Chat screen")
+    }
+
+}
+
+// SettingsScreen 화면 UI
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Text(text = "Settings screen")
+    }
+
 }
 
 
